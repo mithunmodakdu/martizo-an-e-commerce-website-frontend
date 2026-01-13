a590f446eab3cbb94300a134aca0fcdc5cc0572f
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-01-13 23:31:40 +0600

    Building the Dashboard Using ShadCN UI Block sidebar

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,9 +1,34 @@
-import { Outlet } from "react-router";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import { AppSidebar } from "../ui/app-sidebar"
+import { Outlet } from "react-router"
 
 export default function DashboardLayout() {
   return (
-    <div>
-      <Outlet/>
-    </div>
-  );
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+          <div className="flex items-center gap-2 px-3">
+            <SidebarTrigger />            
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <Outlet/>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
