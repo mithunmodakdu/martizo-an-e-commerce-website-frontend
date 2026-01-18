985c7a4038b5dd4ab0c7e369d0aa5db15e7f00cc
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-01-18 22:37:11 +0600

    AddProductCategoryModal created with ShadCN dialog component

--- src/pages/Admin/ProductCategories.tsx
+++ src/pages/Admin/ProductCategories.tsx
@@ -1,3 +1,4 @@
+import { AddProductCategoryModal } from "@/components/modules/ProductCategory/AddProductCategoryModal";
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useGetProductCategoriesQuery } from "@/redux/features/productCategories/productCategories.api";
@@ -5,12 +6,12 @@ import { Edit2, Trash2 } from "lucide-react";
 
 export default function ProductCategories() {
   const { data } = useGetProductCategoriesQuery(undefined);
-  console.log(data);
+  // console.log(data);
   return (
     <div className="max-w-4xl w-full mx-auto space-y-5 ">
       <div className="flex justify-between">
         <h1 className="text-2xl font-bold">Product Categories</h1>
-        <Button>Add Category</Button>
+        <AddProductCategoryModal/>
       </div>
       <div className="border-2 border-muted-foreground rounded-md p-5 ">
         <Table>
@@ -25,7 +26,7 @@ export default function ProductCategories() {
           </TableHeader>
           <TableBody>
             {
-              data?.map((item, index) => (
+              data?.map((item: {name: string, slug: string}, index: number) => (
                 <TableRow>
               <TableCell className="font-medium">{index + 1}</TableCell>
               <TableCell>{item.name}</TableCell>
