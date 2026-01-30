2311b23bd3df9c3ed3c95b681eba9c926ca72224
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-01-30 11:54:03 +0600

    Delete UI in products DataTable created

--- src/components/modules/Product/ProductsTable/columns.tsx
+++ src/components/modules/Product/ProductsTable/columns.tsx
@@ -1,9 +1,32 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogMedia,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import type { IProduct } from "@/types";
 import { type ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, Trash2 } from "lucide-react";
+
+const handleDelete = async(productId: string) => {
+  console.log(productId)
+}
 
 export const columns: ColumnDef<IProduct>[] = [
+  {
+    header: "SL No",
+    cell: ({ row }) => {
+      const rowIndex = row.index;
+      return <div className="text-center w-8">{rowIndex + 1}</div>;
+    },
+  },
   {
     accessorKey: "title",
     header: ({ column }) => {
@@ -17,24 +40,28 @@ export const columns: ColumnDef<IProduct>[] = [
         </Button>
       );
     },
+    // cell: ({row}) => {
+    //   const cellValue = row.getValue("title")
+    //   return <div className="bg-red-600 w-[200px]">{cellValue}</div>
+    // }
   },
   {
     accessorKey: "category",
     header: "Category",
-    cell: ({row}) => {
+    cell: ({ row }) => {
       const categoryObject = row.getValue("category");
-      
-      return <div>{categoryObject?.name}</div>
-    }
+
+      return <div>{categoryObject?.name}</div>;
+    },
   },
   {
     accessorKey: "brand",
     header: "Brand",
-    cell: ({row}) => {
+    cell: ({ row }) => {
       const brandObject = row.getValue("brand");
 
-      return <div>{brandObject?.name}</div>
-    }
+      return <div>{brandObject?.name}</div>;
+    },
   },
   {
     accessorKey: "price",
@@ -42,12 +69,12 @@ export const columns: ColumnDef<IProduct>[] = [
       return (
         <div className="flex justify-end">
           <Button
-          variant="ghost"
-          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-        >
-          Price
-          <ArrowUpDown className="ml-2 h-4 w-4" />
-        </Button>
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Price
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
         </div>
       );
     },
@@ -78,4 +105,49 @@ export const columns: ColumnDef<IProduct>[] = [
     accessorKey: "stock",
     header: "Stock",
   },
+  {
+    accessorKey: "_id",
+    header: "Delete",
+    cell: ({row}) => {
+     
+      return (
+        <div>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive" className="hoover: cursor-pointer">
+                <Trash2 />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent size="sm">
+              <AlertDialogHeader>
+                <AlertDialogMedia className="bg-destructive/10 text-destructive dark:bg-destructive/20 dark:text-destructive">
+                  <Trash2 />
+                </AlertDialogMedia>
+                <AlertDialogTitle>Are you sure to Delete it?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This will permanently delete this. You won't be able to revert
+                  this!
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel variant="outline">
+                  No, cancel!
+                </AlertDialogCancel>
+                <AlertDialogAction variant="destructive">
+                  <Button
+                    variant="destructive"
+                    className="hoover: cursor-pointer"
+                    onClick={() => handleDelete(row.getValue("_id"))}
+                  >
+                    Yes, delete it!
+                  </Button>
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+      );
+    },
+  },
+
 ];
