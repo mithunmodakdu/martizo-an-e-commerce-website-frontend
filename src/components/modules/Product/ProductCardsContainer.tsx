2a5e549fc3f4feb611308b86250bdd7f0ac632ed
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-02-09 15:28:59 +0600

    ProductDetails UI created

--- src/components/modules/Product/ProductCardsContainer.tsx
+++ src/components/modules/Product/ProductCardsContainer.tsx
@@ -12,6 +12,7 @@ import { Button } from "@/components/ui/button";
 import { useGetAllProductsQuery } from "@/redux/features/products/products.api";
 import type { IProduct } from "@/types";
 import { Price, PriceValue } from "./Price";
+import { Link } from "react-router";
 
 interface ProductPrice {
   regular: number;
@@ -39,46 +40,51 @@ type TProductCards = Array<IProductCard>;
 
 interface IProductCardsContainerProps {
   className?: string;
+  productsData?: IProduct[];
 }
 
-export const ProductCardsContainer = ({ className }: IProductCardsContainerProps) => {
-  const { data : productsData } = useGetAllProductsQuery(undefined);
+export const ProductCardsContainer = ({
+  className,
+  productsData,
+}: IProductCardsContainerProps) => {
+  console.log(className, productsData);
 
-  const productCardsData: TProductCards = productsData?.data?.map((item: IProduct) => ({
-    name: item.title,
-    image: {
-      src: item.thumbnail,
-      alt: `Thumbnail of ${item.title}`,
-    },
-    description:
-      item.description.length > 25
-        ? item.description.slice(0, 25)
-        : item.description,
-    price: {
-      regular: item.price,
-      sale: item.salePrice,
-      currency: "USD",
-    },
-    badges: [
-      {
-        text: item?.isFlashSale && "Flash Sale",
-        color: item?.isFlashSale && "oklch(0.577 0.245 27.325)",
+  const productCardsData: TProductCards = productsData?.map(
+    (item: IProduct) => ({
+      name: item.title,
+      image: {
+        src: item.thumbnail,
+        alt: `Thumbnail of ${item.title}`,
       },
-      {
-        text: item?.isMartizoExclusive && "Exclusive",
-        color: item?.isMartizoExclusive && "oklch(0.5941 0.1635 150.03)",
+      description:
+        item.description.length > 25
+          ? item.description.slice(0, 25)
+          : item.description,
+      price: {
+        regular: item.price,
+        sale: item.salePrice,
+        currency: "USD",
       },
-      {
-        text: item?.isTrending && "Trending",
-        color: item?.isTrending && "oklch(0.841 0.238 128.85)",
-      },
-      {
-        text: item?.isNewArrival && "New",
-        color: item?.isNewArrival && "oklch(0.448 0.119 151.328)",
-      },
-    ],
-  }));
-
+      badges: [
+        {
+          text: item?.isFlashSale && "Flash Sale",
+          color: item?.isFlashSale && "oklch(0.577 0.245 27.325)",
+        },
+        {
+          text: item?.isMartizoExclusive && "Exclusive",
+          color: item?.isMartizoExclusive && "oklch(0.5941 0.1635 150.03)",
+        },
+        {
+          text: item?.isTrending && "Trending",
+          color: item?.isTrending && "oklch(0.841 0.238 128.85)",
+        },
+        {
+          text: item?.isNewArrival && "New",
+          color: item?.isNewArrival && "oklch(0.448 0.119 151.328)",
+        },
+      ],
+    }),
+  );
 
   return (
     <section className={cn("px-10 py-32", className)}>
@@ -104,7 +110,7 @@ const ProductCard = ({
 
   return (
     <div className="relative group ">
-      <Card className="min-w-[300px] min-h-[400px] overflow-hidden p-0 rounded-tl-none rounded-tr-3xl rounded-bl-3xl rounded-br-3xl">
+      <Card className="min-w-[350px] min-h-[420px] overflow-hidden p-0 rounded-tl-none rounded-tr-3xl rounded-bl-3xl rounded-br-3xl">
         <CardHeader className="relative block p-0">
           <AspectRatio ratio={1.5} className="overflow-hidden">
             <img
@@ -154,15 +160,19 @@ const ProductCard = ({
         className="absolute min-w-[300px] min-h-[400px] p-0 rounded-tl-none rounded-tr-3xl rounded-bl-3xl rounded-br-3xl inset-0 bg-muted-foreground flex items-center justify-center gap-3
         opacity-0 group-hover:opacity-80 transition-all duration-300 ease-in-out"
       >
-        <Button className=" transform translate-y-24 group-hover:translate-y-0 transition-transform duration-500 ease-in-out ">
-          Add to Cart
-        </Button>
-        <Button
-          variant={"secondary"}
-          className="transform -translate-y-24 group-hover:translate-y-0 transition-transform duration-500 ease-in-out"
-        >
-          View Details
-        </Button>
+        <Link to={"/cart"} className="asChild">
+          <Button className="transform translate-y-24 group-hover:translate-y-0 transition-transform duration-500 ease-in-out ">
+            Add to Cart
+          </Button>
+        </Link>
+        <Link to={"/product-details"} className="asChild">
+          <Button
+            variant={"secondary"}
+            className="transform -translate-y-24 group-hover:translate-y-0 transition-transform duration-500 ease-in-out"
+          >
+            View Details
+          </Button>
+        </Link>
       </div>
     </div>
   );
---
+++ src/pages/ProductDetailsPage.tsx
@@ -0,0 +1,10 @@
+import { ProductDetails } from "@/components/modules/Product/ProductDetails";
+
+
+export default function ProductDetailsPage() {
+  return (
+    <div>
+      <ProductDetails/>
+    </div>
+  );
+}
