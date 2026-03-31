079825bb36cd38fc9301ed1f796c59c18b9b6275
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-04-01 00:54:53 +0600

    Products filter by category created using URL searchParams

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -10,70 +10,79 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import { useState } from "react";
-
-export const title = "Disabled State";
+import { useSearchParams } from "react-router";
+import ProductsFilter from "@/components/modules/Product/ProductsFilter";
 
 export default function ProductsPage() {
   const [currentPage, setCurrentPage] = useState(1);
-  console.log(currentPage);
+  const [searchParams] = useSearchParams();
+
+  const selectedCategory = searchParams.get("category") || undefined;
+  console.log()
+
   const { data: productsData, isLoading } = useGetAllProductsQuery({
     page: currentPage,
     limit: 10,
+    category: selectedCategory
   });
 
   const totalPage = productsData?.meta?.totalPage || 1;
-  console.log(totalPage);
 
   return (
     <div>
+      <div className="w-full flex justify-center items-center mt-10">
+        <ProductsFilter/>
+      </div>
+
       {isLoading ? (
         <Loading />
       ) : (
         <ProductCardsContainer productsData={productsData?.data} />
       )}
-      {
-        totalPage > 1 && (
-          <div>
-        <Pagination>
-          <PaginationContent>
-            <PaginationItem>
-              <PaginationPrevious
-                onClick={() => setCurrentPage((prev) => prev - 1)}
-                className={
-                  currentPage === 1
-                    ? "pointer-events-none opacity-50"
-                    : "cursor-pointer"
-                }
-              />
-            </PaginationItem>
 
-            {Array.from(
-              { length: totalPage },
-              (_, index) => index + 1,
-            ).map((page, index) => (
-              <PaginationItem key={index}>
-                <PaginationLink onClick={() => setCurrentPage(page)} isActive={page === currentPage} className="cursor-pointer">
-                  {page}
-                </PaginationLink>
+      {totalPage > 1 && (
+        <div>
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious
+                  onClick={() => setCurrentPage((prev) => prev - 1)}
+                  className={
+                    currentPage === 1
+                      ? "pointer-events-none opacity-50"
+                      : "cursor-pointer"
+                  }
+                />
               </PaginationItem>
-            ))}
 
-            <PaginationItem>
-              <PaginationNext
-                onClick={() => setCurrentPage((prev) => prev + 1)}
-                className={
-                  currentPage === totalPage
-                    ? "pointer-events-none opacity-50"
-                    : "cursor-pointer"
-                }
-              />
-            </PaginationItem>
-            
-          </PaginationContent>
-        </Pagination>
-      </div>
-        )
-      }
+              {Array.from({ length: totalPage }, (_, index) => index + 1).map(
+                (page, index) => (
+                  <PaginationItem key={index}>
+                    <PaginationLink
+                      onClick={() => setCurrentPage(page)}
+                      isActive={page === currentPage}
+                      className="cursor-pointer"
+                    >
+                      {page}
+                    </PaginationLink>
+                  </PaginationItem>
+                ),
+              )}
+
+              <PaginationItem>
+                <PaginationNext
+                  onClick={() => setCurrentPage((prev) => prev + 1)}
+                  className={
+                    currentPage === totalPage
+                      ? "pointer-events-none opacity-50"
+                      : "cursor-pointer"
+                  }
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        </div>
+      )}
     </div>
   );
 }
