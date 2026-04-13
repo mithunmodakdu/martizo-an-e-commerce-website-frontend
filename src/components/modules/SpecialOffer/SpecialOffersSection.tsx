41fe9feda367e49b404ee58dec719bff5625844b
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-04-13 17:20:50 +0600

    smallDiscountProductCards created

--- src/components/modules/SpecialOffer/SpecialOffersSection.tsx
+++ src/components/modules/SpecialOffer/SpecialOffersSection.tsx
@@ -7,10 +7,59 @@ import type { IProduct } from "../Product/product.types";
 export default function SpecialOffersSection() {
   const { data: topDiscountProducts } = useGetAllProductsQuery({
     sort: "-discountPercentage",
+    page:1,
     limit: 2,
   });
-  console.log(topDiscountProducts);
+
+  const { data: smallDiscountProducts } = useGetAllProductsQuery({
+    sort: "-discountPercentage",
+    page:2,
+    limit: 4
+    
+  });
+  // console.log(topDiscountProducts);
+  // console.log(smallDiscountProducts);
   const topDiscountProductCardsData = topDiscountProducts?.data?.map(
+    (item: IProduct) => ({
+      _id: item._id,
+      name: item.title,
+      slug: item.slug,
+      category: item.category,
+      image: {
+        src: item.thumbnail,
+        alt: `Thumbnail of ${item.title}`,
+      },
+      description:
+        item.description.length > 25
+          ? item.description.slice(0, 25)
+          : item.description,
+      price: {
+        regular: item.price,
+        sale: item.salePrice,
+        currency: "BDT",
+      },
+      variants: item.variants,
+      badges: [
+        {
+          text: item?.isFlashSale && "Flash Sale",
+          color: item?.isFlashSale && "oklch(0.577 0.245 27.325)",
+        },
+        {
+          text: item?.isMartizoExclusive && "Exclusive",
+          color: item?.isMartizoExclusive && "oklch(0.5941 0.1635 150.03)",
+        },
+        {
+          text: item?.isTrending && "Trending",
+          color: item?.isTrending && "oklch(0.841 0.238 128.85)",
+        },
+        {
+          text: item?.isNewArrival && "New",
+          color: item?.isNewArrival && "oklch(0.448 0.119 151.328)",
+        },
+      ],
+    }),
+  );
+  const smallDiscountProductCardsData = smallDiscountProducts?.data?.map(
     (item: IProduct) => ({
       _id: item._id,
       name: item.title,
@@ -82,6 +131,18 @@ export default function SpecialOffersSection() {
           <ProductCard key={item._id} {...item} />
         ))}
       </div>
+      
+      {/* Small deals */}
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3 sm:gap-4">
+        {
+          smallDiscountProductCardsData?.map(item => (
+            <ProductCard key={item._id} {...item}/>
+          ))
+        }
+      </div>
+
+
+
     </section>
   );
 }
