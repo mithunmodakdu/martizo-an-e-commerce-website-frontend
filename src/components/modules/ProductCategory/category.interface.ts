4914a08e6c763ea26eaf6e5a03eafa8a50fd40ed
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-05-20 20:58:14 +0600

    CreateCategoryZodSchema, UpdateCategoryZodSchema created and implemented

--- src/components/modules/ProductCategory/category.interface.ts
+++ src/components/modules/ProductCategory/category.interface.ts
@@ -1,6 +1,33 @@
+import z from "zod";
+
 export interface ICategory {
   _id?: string;
   name?: string;
   parent?: string | null;
   icon?: string;
 }
+
+export const CreateCategoryZodSchema = z.object({
+  name: z
+    .string({ error: "Category name must be string" })
+    .min(2, { error: "Category name must be of at least 2 characters" }),
+  slug: z
+    .string()
+    .min(3, { error: "Category slug must be of at least 3 characters" })
+    .optional(),
+  parent: z.union([z.string(), z.null()], {error:"Parent Category must be string or null"}).optional(),
+  icon: z.string().optional(),
+});
+
+export const UpdateCategoryZodSchema = z.object({
+  name: z
+    .string({ error: "Category name must be string" })
+    .min(2, { error: "Category name must be of at least 2 characters" })
+    .optional(),
+  slug: z
+    .string()
+    .min(3, { error: "Category slug must be of at least 3 characters" })
+    .optional(),
+  parent: z.union([z.string(), z.null()], {error:"Parent Category must be string or null"}).optional(),
+  icon: z.string().optional(),
+});
